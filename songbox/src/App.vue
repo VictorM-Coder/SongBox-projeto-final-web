a9b04b8ce8e7d3d125f31c7b0b0eb9b0ed9963ac
victor <martinsvictor6781@gmail.com>
2023-04-29 16:50:00 -0300

    main

--- songbox/src/App.vue
+++ songbox/src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 
 import MyHeader from "@/components/header/MyHeader.vue";
+import StartNow from "@/components/home/StartNow.vue";
 </script>
 
 <template>
-  <MyHeader></MyHeader>
+  <div>
+    <MyHeader></MyHeader>
+    <start-now></start-now>
+  </div>
 </template>
 
 <style scoped>
-
+div {
+  background: radial-gradient(50% 50% at 50% 50%, rgb(198 201 242 / 12%) 0%, rgba(16, 16, 18, 0) 100%);
+}
 </style>
---
+++ songbox/src/components/home/StartNow.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="container">
+    <h1><strong>Avalie</strong> suas músicas favoritas, <strong>crie playlists</strong> e <strong>compartilhe</strong> sua opinião com todos </h1>
+    <div class="d-flex justify-content-center">
+      <button class="btn-song">comece agora</button>
+    </div>
+  </main>
+</template>
+
+<script>
+  export default{
+    name: "StartNow",
+
+  }
+</script>
+
+<style scoped>
+main {
+  margin-top: 8em;
+}
+
+
+h1{
+  text-align: center;
+  font-weight: 700;
+}
+strong{
+  color: #7882E0;
+  font-weight: 700;
+}
+
+main > div{
+  margin-top: 3em;
+  padding-bottom: 3em;
+}
+</style>
