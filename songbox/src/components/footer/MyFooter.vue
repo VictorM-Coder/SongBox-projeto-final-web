5c445c829fe8719c3a36bb56b12cf42ee4eb2ce1
victor <martinsvictor6781@gmail.com>
2023-05-01 10:20:04 -0300

    init footer

--- songbox/src/components/footer/MyFooter.vue
+++ songbox/src/components/footer/MyFooter.vue
@@ -8,7 +8,15 @@
           </i>
         </div>
 
-        <nav class="col-4">
+        <div class="col-8 col-md-4 order-md-last">
+          <div class="d-flex justify-content-end">
+            <a class="m-12 d-flex"><i class="bi bi-github icon-song link-social"></i></a>
+            <a class="m-12 d-flex"><i class="bi bi-instagram icon-song link-social"></i></a>
+            <a class="m-12 d-flex"><i class="bi bi-linkedin icon-song link-social"></i></a>
+          </div>
+        </div>
+
+        <nav class="col-4 mt-4 mt-md-0">
           <h5>LINKS ÚTEIS</h5>
           <ul>
             <li>
@@ -24,6 +32,7 @@
         </nav>
 
       </div>
+      <div class="row"><p>SongBox © Alguns direitos reservados</p></div>
     </div>
   </footer>
 </template>
@@ -38,6 +47,38 @@ footer {
 ul {
   list-style: none;
   padding: 0;
+}
+
+h5 {
+  margin-bottom: 1em;
+}
+
+li {
+  margin-bottom: 0.8em;
+}
+
+a {
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  text-decoration: none;
+  color: var(--p0-primary-color);
+}
+
+p {
+  margin-top: 4em;
+  margin-bottom: 0;
+  font-family: 'Roboto', sans-serif;
+}
+
+.link-social {
+  background-color: var(--b1-primary-color);
+  padding: 12px;
+  border-radius: 50%;
+  line-height: 0;
+}
+
+.m-12{
+  margin-left: 1em;
 }
 
 </style>
