6092beaa63aa8f9f02e6a26dd57da46db72bb6c1
victor <martinsvictor6781@gmail.com>
2023-06-18 16:23:35 -0300

    autenticação da aba de adm

--- songbox/src/router/index.ts
+++ songbox/src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import MusicPage from "@/views/MusicPage.vue";
 import Login from "@/views/Login.vue";
 import Admin from "@/views/Admin.vue";
+import {useUserStore} from "@/stores/userStore";
 
 const routes: RouteRecordRaw[] = [
     {
@@ -25,13 +26,24 @@ const routes: RouteRecordRaw[] = [
         path: '/admin',
         name: 'admin',
         component: Admin,
-        meta: { permissions: ['admin'] }
+        meta: { permissions: ['admin'], requiresAuth: true }
     }
 ];
 
 const router = createRouter({
     history: createWebHistory(''),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+    const isAuthenticated = (useUserStore().user.username);
+    if (requiresAuth && !isAuthenticated) {
+        next('/login');
+        console.log("nao deu")
+    } else {
+        next();
+    }
 });
 
 export default router;
