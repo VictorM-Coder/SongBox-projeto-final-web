60f72f387c4817db37fc8a030c206a2b74662a11
victor <martinsvictor6781@gmail.com>
2023-06-20 16:21:17 -0300

    inicialização do review form

---
+++ songbox/src/components/forms/ReviewForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="modal fade modal-lg" tabindex="-1" aria-labelledby="musicModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content bg-s0">
+        <div class="modal-header border-0">
+          <div class="container g-0">
+            <div class="row">
+              <div class="col-12 col-md-4 col-lg-3 d-flex justify-content-center">
+                <div class="img-card">
+                  <img class="img-fluid s-border-radius shadow-default"  :src="useUploadFile(props.music.cover.url)" alt="capa do álbum">
+                </div>
+              </div>
+              <div class="col-12 col-md-6 d-flex align-items-end mt-4 mt-md-0">
+                <div>
+                  <p class="music-data text-uppercase mb-2">I LISTENED</p>
+                  <h2 class="mb-1">{{ props.music.title }}</h2>
+                  <span class="music-data d-flex align-items-center">
+                      <span>{{ props.music.artist.name }}</span>
+                      <i class="bi bi-circle-fill divider"></i>
+                      <span> {{ props.music.releaseYear }}</span>
+                      <i class="bi bi-circle-fill divider"></i>
+                      <span> {{ `${props.music.minutesDuration}:${props.music.secondsDuration}` }}</span>
+                    </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-body border-0">
+          <form>
+            <div class="mb-3">
+              <label for="rating-bar" class="col-form-label">Rating:</label>
+              <rating-bar-dynamic id="rating-bar"></rating-bar-dynamic>
+            </div>
+            <div class="mb-3">
+              <label for="recipient-name" class="col-form-label">Title:</label>
+              <input v-model="review.title" type="text" class="form-control" id="recipient-name">
+            </div>
+            <div class="mb-3">
+              <label for="message-text" class="col-form-label">Review:</label>
+              <textarea v-model="review.review" class="form-control" id="message-text"></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="tags-input" class="col-form-label">Tags:</label>
+              <vue3-tags-input
+                  id="tags-input"
+                  :tags="tags"
+                  placeholder="add tags"
+                  @on-tags-changed="handleChangeTag"
+              />
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer border-0">
+          <button type="button" class="btn-crud-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn-crud">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {Music} from "@/model/Music";
+import {useUploadFile} from "@/utils/useUploadURL";
+import {ref} from "vue";
+// @ts-ignore
+import Vue3TagsInput from 'vue3-tags-input';
+import RatingBarDynamic from "@/components/rating-bar/RatingBarDynamic.vue";
+import type {Review} from "@/model/Review";
+
+const tags = ref<string[]>([]);
+const review = ref<Review>({} as Review)
+const props = defineProps({
+  music: Music
+})
+
+function handleChangeTag(newTags:string[]) {
+  tags.value = newTags
+}
+
+const resetForm = () => {
+  review.value = {} as Review
+  tags.value = []
+}
+
+defineExpose({
+  resetForm,
+})
+
+</script>
--- songbox/src/components/music/MusicDisplay.vue
+++ songbox/src/components/music/MusicDisplay.vue
@@ -40,65 +40,7 @@
       </div>
     </div>
 
-    <div ref="reviewModal" class="modal fade modal-lg" id="musicModal" tabindex="-1" aria-labelledby="musicModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content bg-s0">
-          <div class="modal-header border-0">
-            <div class="container g-0">
-              <div class="row">
-                <div class="col-12 col-md-4 col-lg-3 d-flex justify-content-center">
-                  <div class="img-card">
-                    <img class="img-fluid s-border-radius shadow-default"  :src="useUploadFile(props.music.cover.url)" alt="capa do álbum">
-                  </div>
-                </div>
-                <div class="col-12 col-md-6 d-flex align-items-end mt-4 mt-md-0">
-                  <div>
-                    <p class="music-data text-uppercase mb-2">I LISTENED</p>
-                    <h2 class="mb-1">{{ props.music.title }}</h2>
-                    <span class="music-data d-flex align-items-center">
-                      <span>{{ props.music.artist.name }}</span>
-                      <i class="bi bi-circle-fill divider"></i>
-                      <span> {{ props.music.releaseYear }}</span>
-                      <i class="bi bi-circle-fill divider"></i>
-                      <span> {{ `${props.music.minutesDuration}:${props.music.secondsDuration}` }}</span>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-body border-0">
-            <form>
-              <div class="mb-3">
-                <label for="rating-bar" class="col-form-label">Rating:</label>
-                <rating-bar-dynamic id="rating-bar"></rating-bar-dynamic>
-              </div>
-              <div class="mb-3">
-                <label for="recipient-name" class="col-form-label">Title:</label>
-                <input type="text" class="form-control" id="recipient-name">
-              </div>
-              <div class="mb-3">
-                <label for="message-text" class="col-form-label">Review:</label>
-                <textarea class="form-control" id="message-text"></textarea>
-              </div>
-              <div class="mb-3">
-                <label for="tags-input" class="col-form-label">Tags:</label>
-                <vue3-tags-input
-                    id="tags-input"
-                    :tags="tags"
-                    placeholder="add tags"
-                    @on-tags-changed="handleChangeTag"
-                />
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer border-0">
-            <button type="button" class="btn-crud-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn-crud">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <review-form id="musicModal" ref="reviewModal" :music="music"></review-form>
   </div>
 </template>
 
@@ -106,27 +48,25 @@
 import {Modal} from "bootstrap";
 import {Music} from "@/model/Music";
 import {useUploadFile} from "@/utils/useUploadURL";
-import {ref} from "vue";
-// @ts-ignore
-import Vue3TagsInput from 'vue3-tags-input';
+import {onMounted, ref} from "vue";
 import RatingBar from "@/components/rating-bar/RatingBar.vue";
-import RatingBarDynamic from "@/components/rating-bar/RatingBarDynamic.vue";
+import ReviewForm from "@/components/forms/ReviewForm.vue";
 
-const tag = ''
-const tags = ref<string[]>([]);
-const reviewModal = ref<Modal>()
+const reviewModal = ref<ReviewForm>()
 
 const props = defineProps({
   music: Music
 })
 
+onMounted(() => {
+  document.getElementById("musicModal")?.addEventListener('hidden.bs.modal', () => {
+    reviewModal.value.resetForm()
+  })
+})
+
 function showMusicModal(){
   const modalInstance = new Modal(document.getElementById("musicModal") as Element);
   modalInstance.show();
-}
-
-function handleChangeTag(newTags:string[]) {
-  tags.value = newTags
 }
 
 </script>
@@ -137,11 +77,6 @@ h1 {
   font-size: 1.5em;
   font-weight: 600;
   letter-spacing: 0.01em;
-}
-
-.music-data {
-  font-size: 1rem;
-  color: var(--p0-primary-color);
 }
 
 @media (min-width: 576px) {
@@ -183,10 +118,5 @@ h2 {
 
 .padding-4em{
   padding-bottom: 4em;
-}
-
-.divider {
-  font-size: 0.4em;
-  margin: 0 12px;
 }
 </style>
